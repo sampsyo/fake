ec52bc6485b2ae5ca1c9c4fd3406eec0ed71c24d
Adrian Sampson <adrian@radbox.org>
2023-05-31 18:23:27 -0700

    More imaginary states

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,22 @@ fn main() {
     println!("Hello, world!");
 
     let mut driver = Driver::default();
+
+    let dahlia = driver.add_state("dahlia", &["fuse"]);
+    let mrxl = driver.add_state("mrxl", &["mrxl"]);
     let calyx = driver.add_state("calyx", &["futil"]);
     let verilog = driver.add_state("verilog", &["sv"]);
 
-    let op = driver.add_op("compile Calyx to Verilog", calyx, verilog, |_| {
-        println!("compiling!");
+    driver.add_op("compile Calyx to Verilog", calyx, verilog, |_| {
+        unimplemented!()
+    });
+    driver.add_op("compile Calyx internally", calyx, calyx, |_| {
+        unimplemented!()
+    });
+    driver.add_op("compile Dahlia", dahlia, calyx, |_| {
+        unimplemented!()
+    });
+    driver.add_op("compile MrXL", mrxl, calyx, |_| {
         unimplemented!()
     });
 }
